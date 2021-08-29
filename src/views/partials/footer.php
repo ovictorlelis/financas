c163bdd67d3d44d96bfe81e155813ff11a7926e8
Victor Lelis <victorlelisdev@gmail.com>
2021-08-29 18:28:14 -0300

    remove parâmetro message da url

--- src/views/partials/footer.php
+++ src/views/partials/footer.php
@@ -1,4 +1,10 @@
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/js/bootstrap.bundle.min.js"></script>
 
 
+<script>
+  url = new URL(window.location.href);
+  url.searchParams.delete("message");
+  window.history.pushState('', '', url);
+</script>
+
 </html>
